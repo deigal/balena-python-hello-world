b93457191abc69c0c329bdda5ff58a82662816ca
David Sotelo <deigal@hotmail.com>
2024-08-20 16:32:46 -0700

    Updated app.py with pbi.py

--- src/app.py
+++ src/app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template
-app = Flask(__name__, template_folder='../views', static_folder='../views/public')
+import requests
+import random
+import datetime
+import time
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+x = range (100)
+url = 'https://api.powerbi.com/beta/f5622c43-8e64-4863-aed4-0cc4908d3c88/datasets/0c21a444-f22c-4e52-831a-23c3374c8da6/rows?experience=power-bi&key=7gjVKD%2F5nm%2BxHV3ynzgitKEIyCJUIOrXT9BYGSp86uCgGViU0MFqexQ3J2RU4GXtdvQW9wmcxAjbUdPqwixqDA%3D%3D'
+
+temp = 30
+
+for n in x:
+    y = random.uniform(-1, 1)
+    temp = temp + y
+    ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+    myobj = [
+    {
+    "Temperature" :temp,
+    "Timestamp" :ts
+    }
+    ]
+    r = requests.post(url, json = myobj)
+    time.sleep(1)
+    print(myobj)
+    print(r.status_code)
