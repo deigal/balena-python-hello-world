6187a32bd3268a2b97ad4953cc4a13a4d2087c0b
David Sotelo <deigal@hotmail.com>
2025-02-22 11:50:56 -0800

    Sending results to blob storage using date hierarchy folder structure

--- src/app.py
+++ src/app.py
@@ -2,25 +2,41 @@ import requests
 import random
 import datetime
 import time
+import json
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 
+# Azure Storage account connection string
+connect_str = 'CONNECTION_STRING'
+container_name = 'CONTAINER_NAME'
 
-x = range (100)
-url = 'https://api.powerbi.com/beta/f5622c43-8e64-4863-aed4-0cc4908d3c88/datasets/0c21a444-f22c-4e52-831a-23c3374c8da6/rows?experience=power-bi&key=7gjVKD%2F5nm%2BxHV3ynzgitKEIyCJUIOrXT9BYGSp86uCgGViU0MFqexQ3J2RU4GXtdvQW9wmcxAjbUdPqwixqDA%3D%3D'
+# Initialize the BlobServiceClient
+blob_service_client = BlobServiceClient.from_connection_string(connect_str)
 
+x = range(1000)
 temp = 30
 
 for n in x:
     y = random.uniform(-1, 1)
     temp = temp + y
-    ts = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    now = datetime.datetime.now()
+    ts = now.strftime('%Y-%m-%d %H:%M:%S')
 
     myobj = [
-    {
-    "Temperature" :temp,
-    "Timestamp" :ts
-    }
+        {
+            "Temperature": temp,
+            "Timestamp": ts
+        }
     ]
-    r = requests.post(url, json = myobj)
+
+    # Create a folder structure based on the current date and time
+    folder_path = now.strftime('temperature_raw/%Y/%m/%d/%H/%M')
+    blob_name = f'{folder_path}/temperature{now.strftime("%S")}.json'
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
+
+    # Upload the JSON object to the blob
+    blob_client.upload_blob(json.dumps(myobj), overwrite=True)
+
+    
     time.sleep(1)
     print(myobj)
-    print(r.status_code)
+    print(f'Uploaded to Azure Blob Storage: {blob_name}')
